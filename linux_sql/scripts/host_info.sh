a503d31faf9642255afd9768c56146929ea4d1c5
Ahmad Zendebudi <ahmadzendebudi@gmail.com>
2023-04-08 18:52:11 +0000

    implementations added

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-lscpu_out=$(lscpu)
-vmstat_out=$(vmstat -s --unit M)
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
 
-#Hardware info
+if [ $# -ne 5 ]; then
+  echo "Illegal number of parameters"
+  exit 1
+fi
+
+
+#Hardware specs
+lscpu_out=$(lscpu)
+vmstat_out=$(vmstat -s --unit K)
 hostname=$(hostname -f)
-cpu_number=$(echo "$lscpu_out" | sed -nr "s/^CPU\(s\):\s*(\d*)/\1/p")
-cpu_architecture=$(echo "$lscpu_out" | sed -nr "s/^Architecture:\s*(\w*)/\1/p")
-cpu_model=$(echo "$lscpu_out" | sed -nr "s/^Model name:\s*(\w*)/\1/p")
-cpu_mhz=$(echo "$lscpu_out" | sed -nr "s/^CPU MHz:\s*(\w*)/\1/p")
-l2_cache=$(echo "$lscpu_out" | sed -nr "s/^L2 cache:\s*(\w*)/\1/p")
-total_mem=$(echo "$vmstat_out" | sed -nr "s/(\w*) total memory/\1/p")
+cpu_number=$(echo "$lscpu_out" | grep -E "^CPU\(s\):" | awk -v col="2" '{print $col}' | xargs) 
+cpu_architecture=$(echo "$lscpu_out" | grep -E "^Architecture:" | awk -v col="2" '{print $col}'| xargs)
+cpu_model=$(echo "$lscpu_out" | sed -nr "s/^Model name:\s*(.*)\s*/\1/p")
+cpu_mhz=$(echo "$lscpu_out" | grep -E "^CPU MHz:" | awk -v col="3" '{print $col}'| xargs)
+l2_cache=$(echo "$lscpu_out" | grep -E "^L2 cache:" | awk -v col="3" '{print $col}'| sed "s/K//g" | sed "s/M/000/g")
+total_mem=$(echo "$vmstat_out" | grep -E "total memory" | awk -v col="1" '{print $col}'| xargs)
 timestamp=$(date -u +"%Y-%m-%d %H:%M:%S")
 
+#Insert into host_info psql table
+insert_statement="INSERT INTO public.host_info\
+(hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, timestamp, total_mem) \
+VALUES('$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache', '$timestamp', '$total_mem')"
 
+export PGPASSWORD=$psql_password
+psql -h "$psql_host" -p "$psql_port" -d "$db_name" -U "$psql_user" -c "$insert_statement"
 
+exit $?
--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -1,7 +1,46 @@
 #!/bin/bash
 
-vmstat_out=$(vmstat -s --unit M)
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
 
-memory_free=$(echo "$vmstat_out" | sed -nr "s/(\w*) free memory/\1/p")
+if [ $# -ne 5 ]; then
+  echo "Illegal number of arguments"
+  exit 1
+fi
 
-echo "$memory_free"
+# Usage info
+vmstat_out=$(vmstat --unit K)
+hostname=$(hostname -f)
+memory_free=$(echo "$vmstat_out" | tail -1 | awk -v col="4" '{print $col}' | xargs)
+cpu_idle=$(echo "$vmstat_out" | tail -1 | awk -v col="15" '{print $col}' | xargs)
+cpu_kernel=$(echo "$vmstat_out" | tail -1 | awk -v col="14" '{print $col}' | xargs)
+disk_io=$(echo "$vmstat_out" | tail -1 | awk -v col="10" '{print $col}' | xargs)
+disk_available=$(df -BM | grep "/dev/sda2" | awk -v col="4" '{print $col}' | sed "s/M//g")
+timestamp=$(date -u +"%Y-%m-%d %H:%M:%S")
+
+# Required for psql commands
+export PGPASSWORD=$psql_password
+
+# Finding the host_id from host_info psql table
+host_id_sql="SELECT id from host_info where hostname = '$hostname'"
+host_id=$(psql -U "$psql_user" -h "$psql_host" -p "$psql_port" -d "$db_name" -c "$host_id_sql")
+
+if [[ $host_id =~ "(0 rows)" ]]; then
+  echo "No entity in host_info table matches hostname: $hostname"
+  echo "Please insert host info in the host_info table first"
+  exit 1
+fi
+
+host_id=$(echo "$host_id" | head -n 3 | tail -n 1 | xargs)
+
+# Inserting usage info into host_usage psql table
+insert_statement="INSERT INTO host_usage\
+(host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available, timestamp) \
+VALUES('$host_id', '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', '$disk_available', '$timestamp')"
+
+psql -U "$psql_user" -h "$psql_host" -p "$psql_port" -d "$db_name" -c "$insert_statement"
+
+exit $?
